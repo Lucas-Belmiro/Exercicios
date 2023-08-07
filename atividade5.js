1a6bf7fe6a52b6e72e11d60bb3bccd88eedbef54
Lucas-Belmiro <103300371+Lucas-Belmiro@users.noreply.github.com>
2023-08-07 16:19:10 -0300

    add activity 5

---
+++ atividade5.js
@@ -0,0 +1,29 @@
+// Você deve criar três listas em JavaScript:
+
+// 1. para guardar os nomes de dez pessoas;
+// 2. para guardar as suas idades;
+// 3. para guardar as suas cores favoritas.
+
+// Em seguida, imprima essas listas. Depois, faça algumas modificações, alterando uma cor e uma idade. Ao terminar, imprima novamente todas as informações das três listas.
+
+// Realize essa atividade, suba esse arquivo em um drive com link público e compartilhe o link no campo ao lado para que outros desenvolvedores possam analisá-lo.
+
+let namePeople = Array(10);
+
+console.log(namePeople);
+
+let agePeople = Array(10);
+
+console.log(agePeople);
+
+let bestColor = Array(10);
+
+console.log(bestColor);
+
+namePeople[1] = "Lucas";
+
+agePeople[1] = 26;
+
+bestColor[1] = "blue";
+
+console.log(namePeople, agePeople, bestColor);
